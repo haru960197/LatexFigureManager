85c1be8fd939fee1eab7443c0e1b5b10dbe3f496
haru96 <ha.brack96@gmail.com>
2023-09-05 21:08:59 +0900

    [update] グループで管理されたfigureの配列を追加

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -9,12 +9,87 @@ import {
   Tooltip
 } from "@chakra-ui/react";
 import { ArrowUpIcon, ArrowDownIcon, DeleteIcon } from "@chakra-ui/icons";
+
+const ImageListItem = ({
+  figure,
+  index,
+  onUpIconClick,
+  onDownIconClick,
+  onDeleteIconClick
+}) => {
+  return (
+    <ListItem
+          id={`item-${figure.id}`}
+          borderWidth="4px"
+          p="4"
+          mt="4"
+          borderRadius="md"
+          borderColor="gray.400"
+        >
+          <Container centerContent>
+            <Tooltip label={figure.object.name} placement="top-end" fontSize="md">
+              <img width="250" src={figure.base64data} />
+            </Tooltip>
+            <Text mb="2">図{index + 1} : {figure.caption}</Text>
+          </Container>
+          <Flex align="center" justify="flex-end">
+            <Tooltip label="クリックでコピー" placement="auto">
+              <Text
+                fontSize="lg"
+                onClick={() => navigator.clipboard.writeText(figure.label)}
+              >label : {figure.label}</Text>
+            </Tooltip>
+            <Spacer />
+            <IconButton
+              ml="4"
+              icon={<ArrowUpIcon />}
+              colorScheme="blue"
+              onClick={onUpIconClick}
+            />
+            <IconButton
+              ml="2"
+              icon={<ArrowDownIcon/>}
+              colorScheme="blue"
+              onClick={onDownIconClick}
+            />
+            <IconButton
+              ml="2"
+              icon={<DeleteIcon color="blackAlpha.900"/>}
+              bg="gray.400"
+              onClick={onDeleteIconClick}
+            />
+          </Flex>
+        </ListItem>
+  )
+}
+
+function divideIntoGroup(figureList) {
+  const organizedList = [];
+  let bufferList = [];
+  let groupId = '';
+  
+  figureList.forEach((figure, index) => {
+    if (figure.groupId == groupId) {
+      bufferList.push(figure);
+    } else {
+      if (index != 0) {
+        organizedList.push(bufferList);
+      }
+      bufferList = [figure];
+      groupId = figure.groupId;
+    }
+  })
+
+  return organizedList;
+}
+
 export const ImageList = ({
   figureList,
   upperShiftFigureListItem,
 	lowerShiftFigureListItem,
   deleteFigureListItem
-}) => { 
+}) => {
+  const groupedFigureList = divideIntoGroup(figureList);
 
   const handleUpperShift = async (id) => {
     await upperShiftFigureListItem(id);
@@ -36,50 +111,15 @@ export const ImageList = ({
   
   return (
     <List>
-      {figureList.map((imageInfo, index) => (
-        <ListItem 
-          key={imageInfo.id}
-          id={`item-${imageInfo.id}`}
-          borderWidth="4px"
-          p="4"
-          mt="4"
-          borderRadius="md"
-          borderColor="gray.400"
-        >
-          <Container centerContent>
-            <Tooltip label={imageInfo.object.name} placement="top-end" fontSize="md">
-              <img width="250" src={imageInfo.base64data} />
-            </Tooltip>
-            <Text mb="2">図{index + 1} : {imageInfo.caption}</Text>
-          </Container>
-          <Flex align="center" justify="flex-end">
-            <Tooltip label="クリックでコピー" placement="auto">
-              <Text
-                fontSize="lg"
-                onClick={() => navigator.clipboard.writeText(imageInfo.label)}
-              >label : {imageInfo.label}</Text>
-            </Tooltip>
-            <Spacer />
-            <IconButton
-              ml="4"
-              icon={<ArrowUpIcon />}
-              colorScheme="blue"
-              onClick={() => handleUpperShift(imageInfo.id)}
-            />
-            <IconButton
-              ml="2"
-              icon={<ArrowDownIcon/>}
-              colorScheme="blue"
-              onClick={() => handleLowerShift(imageInfo.id)}
-            />
-            <IconButton
-              ml="2"
-              icon={<DeleteIcon color="blackAlpha.900"/>}
-              bg="gray.400"
-              onClick={() => handleDeleteItem(imageInfo.id)}
-            />
-          </Flex>
-        </ListItem>
+      {figureList.map((figure, index) => (
+        <ImageListItem
+          key={figure.id}
+          figure={figure}
+          index={index}
+          onUpIconClick={() => handleUpperShift(figure.id)}
+          onDownIconClick={() => handleLowerShift(figure.id)}
+          onDeleteIconClick={() => handleDeleteItem(figure.id)}
+        />
       ))}
     </List>
   );
